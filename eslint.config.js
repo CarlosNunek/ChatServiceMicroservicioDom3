a2cdd077c98e7930f1c2b120a9aa72718a54700d
CarlosNunek <chnunez@uce.edu.ec>
2025-07-04 18:43:15 -0500

    fix: corregido tests v8

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: js.configs.recommended.languageOptions.globals,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
+];
